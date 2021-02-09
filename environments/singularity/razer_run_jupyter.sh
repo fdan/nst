55d9d4e49041e2f13b4a5da448874ad499dadd8d
Daniel Flood <daniel.flood-1@uts.edu.au>
2021-02-09 11:45:01 +1100

    wip

--- environments/singularity/razer_run_jupyter.sh
+++ environments/singularity/razer_run_jupyter.sh
@@ -3,6 +3,6 @@
 export PYTHONPATH=$PYTHONPATH:/home/dan/git/nst/python
 export NST_VGG_MODEL='/home/dan/git/nst/bin/Models/vgg_conv.pth'
 
-singularity exec nst.sif jupyter lab --notebook-dir /home/dan/git/nst-temporal --allow-root
+singularity exec --nv nst.sif jupyter lab --notebook-dir /home/dan/git/nst-temporal --allow-root
 
 
