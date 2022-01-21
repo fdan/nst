bd853697f0eb422a5289c928605a44a368d384f5
dan.flood <daniel.flood.80@gmail.com>
2022-01-21 19:45:35 +1100

    omit r11 from nst contact sheet to test something

--- python/nst_farm/batch.py
+++ python/nst_farm/batch.py
@@ -11,6 +11,7 @@ except ImportError:
 
 
 VGG_LAYERS = ['r11', 'r12', 'r21', 'r22', 'r31', 'r32', 'r33', 'r34', 'r41', 'r42', 'r43', 'r44', 'r51', 'r52', 'r53', 'r54']
+GATYS_LAYERS = ['r11', 'r31', 'r41', 'r51']
 
 
 def lerp(start, end, step):
@@ -252,7 +253,8 @@ def nst_contact_sheet(style_image, content_image, mips, iterations, engine, outd
             cmd += ['--out', "%s/%s/%s/nst/mip%s/%s.exr" % (outdir, style_image_filename, version, mip, 'gatys_layers')]
             cmd += ['--iterations', iterations]
             cmd += ['--style-scale', scale]
-            cmd += ['--mips', "{\"%s\": {\"r11\": 0.244140625, \"r21\": 0.06103515625, \"r31\": 0.0152587890625, \"r41\": 0.003814697265625, \"r51\": 0.003814697265625}}" % (scale)]
+            # cmd += ['--mips', "{\"%s\": {\"r11\": 0.244140625, \"r21\": 0.06103515625, \"r31\": 0.0152587890625, \"r41\": 0.003814697265625, \"r51\": 0.003814697265625}}" % (scale)]
+            cmd += ['--mips', "{\"%s\": {\"r21\": 0.06103515625, \"r31\": 0.0152587890625, \"r41\": 0.003814697265625, \"r51\": 0.003814697265625}}" % (scale)]
             cmd += ['--clayers', 'r41']
             cmd += ['--cweights', 1.0]
             job.newTask(title="%s_mip_%s_gatysLayers" % (style_image_filename, mip), argv=cmd)
