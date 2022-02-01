b278853e1ae07e5e31ceb77db27648ac5750fe2f
dan.flood <daniel.flood.80@gmail.com>
2022-02-01 22:47:18 +1100

    wip

--- python/nst/utils.py
+++ python/nst/utils.py
@@ -474,8 +474,8 @@ class Pyramid(object):
 
         pyr = []
         pyr.append(img)
-        
-        write_exr(tensor_to_buf(pyr[0]), outdir + '/tmp2.exr')
+        write_exr(tensor_to_buf(img), outdir + '/tmp2.exr')
+        write_exr(tensor_to_buf(pyr[0]), outdir + '/tmp3.exr')
 
         # for level in range(0, max_levels-1):
         #     b, c, old_width, old_height = img.size()
